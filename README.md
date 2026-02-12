d78912f52a31e91ad4c457a82d8b50957c300154
Darshik <darshikladhe182@gmail.com>
2026-02-12 17:50:57 +0530

    Project Initialised

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ### Gamified-Schedule-App
+
+Tailwind works only after this command is typed
+
+npx tailwindcss -i ./src/input.css -o ./src/dist/output.css --watch
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ["./index.html", "./src/js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        mainbg: "#F3F0FF",
+        card: "#FFFFFF",
+        accent: "#7C3AED",
+        secondary: "#60A5FA",
+        xp: "#34D399",
+        health: "#F43F5E",
+        coins: "#FACC15",
+        overdue: "#FB923C",
+        border: "#E5E7EB",
+      },
+    },
+  },
+};
