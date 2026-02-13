f8c74142d87681a92697a237ebb9006ce886ec44
vanshi040907 <vanshi040907@gmail.com>
2026-02-13 19:27:43 +0530

    theme

---
+++ src/js/avatar.js
@@ -0,0 +1,14 @@
+import { loadFromLocalStor, saveToLocalStor } from "./backend.js";
+
+let arr=["Harry Potter", "Dobby", "Ron Weasly", "Albus Dumbeldore", "Rubeus Hagrid", "Draco Malfoy", "Voldemort", "Lucious Malfoy", "Severus Snape", "Minerva McGonagall", "Ginny Weasly", "Hermoine Granger", "Sirius Black"];
+
+let price=[0, 20, 40, 45, 45, 50, 100, 20, 50, 50, 40, 80, 40 ];
+
+let cards=document.querySelectorAll('.profCard');
+
+for(let i=0; i<13; i++){
+    cards[i].addEventListener('click', ()=>{
+        
+    });
+}
+
